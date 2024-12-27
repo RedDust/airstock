2b022eb72310cfd0003d33af7c80d39a8e82a39a
reddust <reddust9@gmail.com>
2024-12-27 13:23:21 +0900

    1. 주식 테이블 기본 설정

---
+++ Stock/CONFIG/ConstTableName.py
@@ -0,0 +1,21 @@
+#initialization of constants
+
+RealtyDatabase = 'stockfriends'
+StockFriendsDatabase = 'stockfriends'
+
+
+
+NaverIndustryCategoryTable = 'stockfriends_naver_industry_category'
+NaverIndustryCategoryLogTable = 'stockfriends_naver_industry_category_log'
+NaverStockItemTable = 'stockfriends_naver_item_list'
+NaverStockItemLogTable = 'stockfriends_naver_items_list_log'
+
+StockItemOriginTable = 'stockfriends_item_ori'
+
+StockItemRootTable = 'stockfriends_item'
+
+
+
+SwitchTable = 'stockfriends_switch'
+
+KoreaInvestOpenCalendarTable = 'stockfriends_koreainvest_open_calendar'
