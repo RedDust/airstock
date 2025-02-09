b9495c2ffe5577118601fddde96e9bdda5c9e090
reddust <reddust9@gmail.com>
2025-02-10 01:23:46 +0900

    1.샘플 코드 커밋

--- Realty/Government/1_seoul_module_sample_20250210.py
+++ Realty/Government/1_seoul_module_sample_20250210.py
@@ -1,6 +1,6 @@
-# 서울 버스 정거장 위경도 데이터
-# 2023-01-30 커밋
-#https://data.seoul.go.kr/dataList/OA-21276/S/1/datasetView.do
+# 지역 실거래 데이터 - 서울(41) 버스 정류장 위도경도 데이터 (75)
+# 2025-02-10 커밋
+# https://data.seoul.go.kr/dataList/OA-12912/S/1/datasetView.do
 
 
 import sys
@@ -16,18 +16,14 @@ import re
 
 sys.path.append("D:/PythonProjects/airstock")
 
-
 import urllib.request
 import json
 import pymysql
 import datetime, time, inspect
 
-
 from Lib.RDB import pyMysqlConnector
 
-
 from Init.Functions.Logs import GetLogDef
-
 
 from datetime import datetime as DateTime, timedelta as TimeDelta
 from Realty.Naver.NaverLib import LibNaverMobileMasterSwitchTable
@@ -37,53 +33,27 @@ from Init.Functions.Logs import GetLogDef as SLog
 from Stock.LIB.Logging import UnifiedLogDeclarationFunction as ULF
 from Lib.CustomException.QuitException import QuitException
 
+
 def main():
-
-
     try:
 
         print("====================== TRY START")
 
+        strProcessType = '034175'
+        data_1 = '00'
+        data_2 = '00'
+        data_3 = '00'
+        data_4 = '00'
+        data_5 = '00'
+        data_6 = '00'
 
-        strProcessType = '999999'
-        strSidoCode = '00'
-        strPageNo = '0'
-        strSidoName = '00'
-        strAddressSiguSequence = '0'
-        nConversionSequence='0'
         dtNow = DateTime.today()
-        dtTimeBefore1Min = DateTime.today() - TimeDelta(seconds=5)
-        strTimeStamp = str(dtTimeBefore1Min.timestamp()).replace(".", "")[0:13]
-        # print(dtNow.hour)
-        # print(dtNow.minute)
-        print("strTimeStamp => " , strTimeStamp)
-
-        LogPath = 'CourtAuction/' + strProcessType
+        LogPath = 'Government/' + strProcessType
         setLogger = ULF.setLogFile(dtNow, logging, LogPath)
         intWeekDay = dtNow.weekday()
 
         logging.info(SLog.Ins(Isp.getframeinfo, Isp.currentframe()),
                      "[CRONTAB START]============================================================")
-
-        # 스위치 데이터 조회 type(000200) result (10:정기점검)
-        rstResult = LibNaverMobileMasterSwitchTable.SwitchResultSelectV2('000200')
-        strResult = rstResult.get('result')
-        if strResult is False:
-            quit(SLog.Ins(Isp.getframeinfo, Isp.currentframe()) + 'strResult => ' + str(strResult))  # 예외를 발생시킴
-
-        if strResult == '10':
-            process_start_date = rstResult.get('process_start_date').strftime('%Y-%m-%d %H:%M:%S')
-            last_date = rstResult.get('last_date').strftime('%Y-%m-%d %H:%M:%S')
-            dtRegNow = DateTime.today()
-            process_start_date_obj = DateTime.strptime(process_start_date, '%Y-%m-%d %H:%M:%S')
-            last_date_obj = DateTime.strptime(last_date, '%Y-%m-%d %H:%M:%S')
-
-            if (process_start_date_obj <= dtRegNow) and (dtRegNow <= last_date_obj):
-                logging.info(
-                    SLog.Ins(Isp.getframeinfo, Isp.currentframe()) + "process_start_date >> " + process_start_date)
-                logging.info(SLog.Ins(Isp.getframeinfo, Isp.currentframe()) + "dtRegNow >> " + dtRegNow)
-                logging.info(SLog.Ins(Isp.getframeinfo, Isp.currentframe()) + "last_date >> " + last_date)
-                quit(SLog.Ins(Isp.getframeinfo, Isp.currentframe()) + 'strResult => ' + strResult)  # 예외를 발생시킴
 
         # 스위치 데이터 조회 type(20=법원경매물건 수집) result (10:처리중, 00:시작전, 20:오류 , 30:시작준비)
         rstResult = LibNaverMobileMasterSwitchTable.SwitchResultSelectV2(strProcessType)
@@ -98,39 +68,15 @@ def main():
         if strResult == '20':
             strAddressSiguSequence = str(rstResult.get('data_1'))
 
-        if strResult == '40':
-            quit(SLog.Ins(Isp.getframeinfo, Isp.currentframe()) + '경매 서비스 점검 ' + str(strResult))  # 예외를 발생시킴
-
         # 스위치 데이터 업데이트 (10:처리중, 00:시작전, 20:오류 , 30:시작준비 - start_time 기록)
         dictSwitchData = dict()
         dictSwitchData['result'] = '10'
-        dictSwitchData['data_1'] = "00"
-        dictSwitchData['data_2'] = "00"
-        dictSwitchData['data_3'] = "00"
-        dictSwitchData['data_4'] = "00"
-        dictSwitchData['data_5'] = "00"
-        dictSwitchData['data_6'] = "00"
+        dictSwitchData['data_1'] = data_1
         LibNaverMobileMasterSwitchTable.SwitchResultUpdateV2(strProcessType, True, dictSwitchData)
-
-
-
-
-
-
-
-
-
-
 
         # 스위치 데이터 업데이트 (10:처리중, 00:시작전, 20:오류 , 30:시작준비 - start_time 기록)
         dictSwitchData = dict()
         dictSwitchData['result'] = '00'
-        dictSwitchData['data_1'] = strSidoCode
-        dictSwitchData['data_2'] = strAddressSiguSequence
-        dictSwitchData['data_3'] = strSidoName
-        dictSwitchData['data_4'] = strPageNo
-        dictSwitchData['data_5'] = nConversionSequence
-        dictSwitchData['data_6'] = strSidoName
         dictSwitchData['today_work'] = '1'
         LibNaverMobileMasterSwitchTable.SwitchResultUpdateV2(strProcessType, False, dictSwitchData)
 
@@ -140,24 +86,30 @@ def main():
     except Exception as e:
 
         # 스위치 데이터 업데이트 (10:처리중, 00:시작전, 20:오류 , 30:시작준비 - start_time 기록)
-        dictSwitchData = dict()
-        dictSwitchData['result'] = '20'
-
-        if strSidoCode is not None:
-            dictSwitchData['data_1'] = strSidoCode
-
-        if strAddressSiguSequence is not None:
-            dictSwitchData['data_2'] = strAddressSiguSequence
-
-        if strSidoName is not None:
-            dictSwitchData['data_3'] = strSidoName
-
-        LibNaverMobileMasterSwitchTable.SwitchResultUpdateV2(strProcessType, False, dictSwitchData)
-
-        err_msg = traceback.format_exc()
+        data_6 = err_msg = traceback.format_exc()
         logging.info(SLog.Ins(Isp.getframeinfo, Isp.currentframe()) + "[=============[Error Exception]")
         logging.info(SLog.Ins(Isp.getframeinfo, Isp.currentframe()) + "[e : (" + str(e) + ")")
         logging.info(SLog.Ins(Isp.getframeinfo, Isp.currentframe()) + "[err_msg : (" + str(err_msg) + ")")
+
+        dictSwitchData = dict()
+        dictSwitchData['result'] = '20'
+
+        if data_1 is not None:
+            dictSwitchData['data_1'] = data_1
+        if data_2 is not None:
+            dictSwitchData['data_2'] = data_2
+        if data_3 is not None:
+            dictSwitchData['data_3'] = data_3
+        if data_4 is not None:
+            dictSwitchData['data_4'] = data_4
+        if data_5 is not None:
+            dictSwitchData['data_5'] = data_5
+        if data_6 is not None:
+            dictSwitchData['data_6'] = data_6
+
+        LibNaverMobileMasterSwitchTable.SwitchResultUpdateV2(strProcessType, False, dictSwitchData)
+
+
 
 
     except QuitException as e:
@@ -166,21 +118,27 @@ def main():
         dictSwitchData = dict()
         dictSwitchData['result'] = '20'
 
-        if strSidoCode is not None:
-            dictSwitchData['data_1'] = strSidoCode
-
-        if strAddressSiguSequence is not None:
-            dictSwitchData['data_2'] = strAddressSiguSequence
-
-        if strSidoName is not None:
-            dictSwitchData['data_3'] = strSidoName
-
-        LibNaverMobileMasterSwitchTable.SwitchResultUpdateV2(strProcessType, False, dictSwitchData)
-
-        err_msg = traceback.format_exc()
+        data_6 = err_msg = traceback.format_exc()
         logging.info(SLog.Ins(Isp.getframeinfo, Isp.currentframe()) + "[=============[Error Exception]")
         logging.info(SLog.Ins(Isp.getframeinfo, Isp.currentframe()) + "[e : (" + str(e) + ")")
         logging.info(SLog.Ins(Isp.getframeinfo, Isp.currentframe()) + "[err_msg : (" + str(err_msg) + ")")
+
+        if data_1 is not None:
+            dictSwitchData['data_1'] = data_1
+        if data_2 is not None:
+            dictSwitchData['data_2'] = data_2
+        if data_3 is not None:
+            dictSwitchData['data_3'] = data_3
+        if data_4 is not None:
+            dictSwitchData['data_4'] = data_4
+        if data_5 is not None:
+            dictSwitchData['data_5'] = data_5
+        if data_6 is not None:
+            dictSwitchData['data_6'] = data_6
+
+        LibNaverMobileMasterSwitchTable.SwitchResultUpdateV2(strProcessType, False, dictSwitchData)
+
+
 
 
     else:
@@ -189,8 +147,6 @@ def main():
     finally:
         print("Finally END")
         # ResRealEstateConnection.close()
-
-
 
 
 if __name__ == '__main__':
